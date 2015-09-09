d047fca1f3eb0b31b7e561ebf3ffb5cd5aa20657
Geert Josten <geert.josten@marklogic.com>
2015-09-09 20:57:58 +0200

    Merged improvements from Matt's ml-lodlive dir, added ExampleProfile service

--- src/ml-lodlive-dir.js
+++ src/ml-lodlive-dir.js
@@ -5,45 +5,22 @@
     .directive('mlLodlive', [function () {
       return {
         restrict: 'E',
-        controller: 'MLLodliveCtrl',
-        //replace: true,
+        replace: true,
         scope: {
-          lodliveId: '@'
+          iri: '@',
+          profile: '='
         },
-        // TODO: get rid of id
-        template: '<div id="mllodlive" class="ml-lodlive"><ml-lodlive-legend></ml-lodlive-legend></div>'
+        template: '<div class="ml-lodlive"><ml-lodlive-legend></ml-lodlive-legend></div>',
+        link: function($scope, $elem, $attrs) {
+
+          $scope.$watch(function() { return $attrs.iri; }, function(newVal, oldVal) {
+            if (newVal) {
+              angular.element($elem).lodlive({ profile: $scope.profile, firstUri: newVal, ignoreBnodes: true });
+            }
+          });
+
+        },
       };
-    }])
-    .controller('MLLodliveCtrl', ['$scope', 'MLRest', function ($scope, mlRest) {
-      var model = {
-        lodlive: {}
-      };
-
-      if ($scope.lodliveId) {
-        model.lodlive.id = $scope.lodliveId;
-        // TODO: use link function, and $elem to get the right element. Directive could be used more often..
-        jQuery('#mllodlive').lodlive(
-          {
-            profile: MarkLogicProfile,
-            firstUri: model.lodlive.id
-          }
-        );
-      }
-
-      $scope.$watch('lodliveId', function(newVal, oldVal) {
-        // TODO: use link function, and $elem to get the right element. Directive could be used more often..
-        var lodliveObj = jQuery('#mllodlive').lodlive();
-        model.lodlive.id = newVal;
-
-        if (lodliveObj) {
-          lodliveObj.context.empty();
-          lodliveObj.init(model.lodlive.id);
-        }
-
-      }, true);
-
-      angular.extend($scope, {
-        model: model
-      });
     }]);
+
 }());
