f2f83e6a4b692c6405fa26bf5dc0075fbde21457
Geert Josten <geert.josten@marklogic.com>
2015-09-10 12:46:44 +0200

    Removed global dependency from legend

--- dist/ml-lodlive-ng.js
+++ dist/ml-lodlive-ng.js
@@ -11,7 +11,7 @@ angular.module('ml.lodlive', ['ml.common', 'ml.lodlive.tpls']);
           iri: '=',
           profile: '='
         },
-        template: '<div class="ml-lodlive"><ml-lodlive-legend></ml-lodlive-legend></div>',
+        template: '<div class="ml-lodlive"><ml-lodlive-legend profile="profile"></ml-lodlive-legend></div>',
         link: function($scope, $elem, $attrs) {
 
           $scope.$watch('iri', function(newVal, oldVal) {
@@ -22,6 +22,108 @@ angular.module('ml.lodlive', ['ml.common', 'ml.lodlive.tpls']);
 
         },
       };
+    }]);
+
+}());
+
+(function () {
+
+  'use strict';
+
+  angular.module('ml.lodlive')
+    .directive('mlLodliveLegend', [function () {
+      return {
+        restrict: 'E',
+        controller: 'MLLodliveLegendCtrl',
+        replace: true,
+        scope: {
+          profile: '='
+        },
+        templateUrl: '/ml-lodlive-ng/ml-lodlive-legend-dir.html'
+      };
+    }])
+    .controller('MLLodliveLegendCtrl', ['$scope', function ($scope) {
+      var model = {
+        profile: $scope.profile,
+        showLegend: false,
+        items: [
+          {
+            icon: 'fa fa-list',
+            title: 'Document Information'
+          },
+          {
+            icon: 'fa fa-cog',
+            title: 'Tools'
+          },
+          {
+            icon: 'fa fa-arrows-alt',
+            title: 'Expand All'
+          },
+          {
+            icon: 'fa fa-info-circle',
+            title: 'More Information'
+          },
+          {
+            icon: 'fa fa-dot-circle-o',
+            title: 'Make Root Node'
+          },
+          {
+            icon: 'fa fa-external-link',
+            title: 'Open In Another Page'
+          },
+          {
+            icon: 'fa fa-trash-o',
+            title: 'Remove the Node'
+          },
+          {
+            icon: 'fa fa-certificate',
+            title: 'Group of Related Items'
+          },
+          {
+            icon: 'fa fa-circle',
+            title: 'Related Item'
+          }
+        ],
+        relationships: []
+      };
+
+      function initRelationships() {
+        // The MarkLogic Lodlive profile is in global scope.
+        if (model.profile.UI && model.profile.UI.relationships) {
+          var rels = model.profile.UI.relationships;
+
+          for (var p in rels) {
+            if (rels.hasOwnProperty(p)) {
+              var title = p;
+              if (p.indexOf('#') > -1) {
+                title = p.substring(p.lastIndexOf('#') + 1);
+              }
+              else if (p.indexOf('/') > -1) {
+                title = p.substring(p.lastIndexOf('/') + 1);
+              }
+
+              model.relationships.push({
+                icon: 'fa fa-circle',
+                title: title,
+                style: 'color: ' + rels[p].color
+              });
+            }
+          }
+        }
+
+        // Add the default color used when a relationship doesn't have a specified color.
+        model.relationships.push({
+          icon: 'fa fa-circle',
+          title: 'other',
+          style: 'color: #369;'
+        });
+      }
+
+      initRelationships();
+
+      angular.extend($scope, {
+        model: model
+      });
     }]);
 
 }());
@@ -236,106 +338,6 @@ angular.module('ml.lodlive', ['ml.common', 'ml.lodlive.tpls']);
         };
       }
     ]);
-
-}());
-
-(function () {
-
-  'use strict';
-
-  angular.module('ml.lodlive')
-    .directive('mlLodliveLegend', [function () {
-      return {
-        restrict: 'E',
-        controller: 'MLLodliveLegendCtrl',
-        replace: true,
-        scope: {},
-        templateUrl: '/ml-lodlive-ng/ml-lodlive-legend-dir.html'
-      };
-    }])
-    .controller('MLLodliveLegendCtrl', ['$scope', 'MLLodliveExampleProfile', function ($scope, profile) {
-      var model = {
-        profile: profile,
-        showLegend: false,
-        items: [
-          {
-            icon: 'fa fa-list',
-            title: 'Document Information'
-          },
-          {
-            icon: 'fa fa-cog',
-            title: 'Tools'
-          },
-          {
-            icon: 'fa fa-arrows-alt',
-            title: 'Expand All'
-          },
-          {
-            icon: 'fa fa-info-circle',
-            title: 'More Information'
-          },
-          {
-            icon: 'fa fa-dot-circle-o',
-            title: 'Make Root Node'
-          },
-          {
-            icon: 'fa fa-external-link',
-            title: 'Open In Another Page'
-          },
-          {
-            icon: 'fa fa-trash-o',
-            title: 'Remove the Node'
-          },
-          {
-            icon: 'fa fa-certificate',
-            title: 'Group of Related Items'
-          },
-          {
-            icon: 'fa fa-circle',
-            title: 'Related Item'
-          }
-        ],
-        relationships: []
-      };
-
-      function initRelationships() {
-        // The MarkLogic Lodlive profile is in global scope.
-        if (model.profile.UI && model.profile.UI.relationships) {
-          var rels = model.profile.UI.relationships;
-
-          for (var p in rels) {
-            if (rels.hasOwnProperty(p)) {
-              var title = p;
-              if (p.indexOf('#') > -1) {
-                title = p.substring(p.lastIndexOf('#') + 1);
-              }
-              else if (p.indexOf('/') > -1) {
-                title = p.substring(p.lastIndexOf('/') + 1);
-              }
-
-              model.relationships.push({
-                icon: 'fa fa-circle',
-                title: title,
-                style: 'color: ' + rels[p].color
-              });
-            }
-          }
-        }
-
-        // Add the default color used when a relationship doesn't have a specified color.
-        model.relationships.push({
-          icon: 'fa fa-circle',
-          title: 'other',
-          style: 'color: #369;'
-        });
-      }
-
-      initRelationships();
-
-      angular.extend($scope, {
-        model: model
-      });
-    }]);
 
 }());
 
